e3a149debf21a2b803d88f9d80a21ff6921f029b
MehrinFirdousi <mehrinfirdousi@gmail.com>
2023-02-24 19:23:15 +0400

    Added WSGI toFlask app, socket is pending

--- app.py
+++ app.py
@@ -9,30 +9,23 @@ import cv2
 import mediapipe as mp
 from flask import Flask, request
 
-model = None
 app = Flask(__name__)
 
-# def load_model():
-# 	global model
-# 	with open('iris_trained_model.pkl', 'rb') as f:
-# 		model = pickle.load(f);
+global model
+# global frame_paths
+global frame_rate
+global mp_holistic
+global mp_drawing
+global actions
+global colors
 
-def load_model():
-    global model
-    # global frame_paths
-    global frame_rate
-    global mp_holistic
-    global mp_drawing
-    global actions
-    global colors
-
-    # frame_paths = []
-    frame_rate = 25
-    mp_holistic = mp.solutions.holistic # Holistic model - make our detection
-    mp_drawing = mp.solutions.drawing_utils # Drawing utilities - make our drawings
-    actions = np.array(['NoSign','hello', 'thanks', 'iloveyou'])
-    colors = [(245,117,16), (117,245,16), (16,117,245),(16,117,245)]
-    model = keras.models.load_model('test8.h5')
+# frame_paths = []
+frame_rate = 25
+mp_holistic = mp.solutions.holistic # Holistic model - make our detection
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities - make our drawings
+actions = np.array(['NoSign','hello', 'thanks', 'iloveyou'])
+colors = [(245,117,16), (117,245,16), (16,117,245),(16,117,245)]
+model = keras.models.load_model('test8.h5')
 
 @app.route('/')
 def home_endpoint():
@@ -77,10 +70,10 @@ def run_model():
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         
         all_frames = os.listdir('./frames')
-        new_list = all_frames[-frame_rate:]
+        new_list = all_frames[-25:]
         
-        # new_list = os.listdir('frames')[-frame_rate:]
-        # new_list = frame_paths[-frame_rate:]
+        # new_list = os.listdir('frames')[-25:]
+        # new_list = frame_paths[-25:]
         imgcount = len(new_list)
         for i in range(imgcount):
             # Read feed
@@ -96,9 +89,9 @@ def run_model():
             # Prediction logic
             keypoints = extract_keypoints(results)
             sequence.append(keypoints)
-            sequence = sequence[-frame_rate:]
+            sequence = sequence[-25:]
             
-            if len(sequence) == frame_rate:
+            if len(sequence) == 25:
                 res = model.predict(np.expand_dims(sequence, axis=0))[0]
                 print(actions[np.argmax(res)])
                 result_p = actions[np.argmax(res)]
@@ -115,7 +108,7 @@ def get_image(frameCount):
     # # print("\nReceived image File name : ", frame_paths[-1])
     # imagefile.save("frames/" + filename)
     # print("\nReceived", len(frame_paths), "frames")
-    # if len(frame_paths) % frame_rate == 0:
+    # if len(frame_paths) % 25 == 0:
     #     return(run_model())
     # if len(frame_paths)>=500:
     #     # for i in range(50):
@@ -130,7 +123,7 @@ def get_image(frameCount):
     # print("\nReceived image File name : ", frame_paths[-1])
     imagefile.save("frames/" + filename)
     print("\nReceived", frameCount, "frames")
-    if count % frame_rate == 0:
+    if count % 25 == 0:
         return(run_model())
     return ("nothing")
 
@@ -148,5 +141,5 @@ def test_endpoint(name):
     return name
 
 if __name__ == '__main__':
-    load_model()  # load model at the beginning once only
+    # load_model()  # load model at the beginning once only
     app.run(host='0.0.0.0', port=5000)
