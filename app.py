48ea962e0a7ecc435a84ced4746b636dd419cb72
MehrinFirdousi <mehrinfirdousi@gmail.com>
2023-03-07 13:48:55 +0400

    port change

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def test_endpoint(name):
 
 if __name__ == '__main__':
     # load_model()  # load model at the beginning once only
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
